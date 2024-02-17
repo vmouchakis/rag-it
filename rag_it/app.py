8993d96a4224e352dfa07ac3ae0e0491e93becaa
vmouchakis <vmouchakis@gmail.com>
2024-02-17 13:25:23 +0200

    init commit, use llama2 7b to generate response

---
+++ rag_it/app.py
@@ -0,0 +1,30 @@
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+
+
+llm = LlamaCPP(
+    # You can pass in the URL to a GGML model to download it automatically
+    model_url=None,
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    model_path="models/7b-ggml-model-f32_q4_0.bin",
+    temperature=0.1,
+    max_new_tokens=256,
+    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    context_window=3900,
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # kwargs to pass to __init__()
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": 1},
+    # transform inputs into Llama2 format
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=False,
+)
+
+response_iter = llm.stream_complete("can you please create a python for loop?")
+for response in response_iter:
+    print(response.delta, end="", flush=True)
